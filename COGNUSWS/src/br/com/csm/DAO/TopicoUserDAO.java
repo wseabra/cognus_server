e04eab8edc08e5764e0f34457784a20074e36af1
Maroo <damhur92@gmail.com>
2017-07-16 23:44:50 -0400

    Commit server mario

--- COGNUSWS/src/br/com/csm/DAO/TopicoUserDAO.java
+++ COGNUSWS/src/br/com/csm/DAO/TopicoUserDAO.java
@@ -1,42 +1,42 @@
-package br.com.csm.DAO;
-
-import java.util.List;
-
-import javax.persistence.EntityManager;
-import javax.persistence.Query;
-
-import br.com.csm.entidade.Topico;
-import br.com.csm.entidade.TopicoUser;
-
-public class TopicoUserDAO {
-
-	private EntityManager entityManager;
-	
-	public TopicoUserDAO(EntityManager entityManager){
-		this.entityManager = entityManager;
-	}
-	
-	public void cadastrar(TopicoUser topico){
-		entityManager.persist(topico);
-	}
-	
-	public void alterar(TopicoUser topico){
-		entityManager.merge(topico);
-	}
-	
-	public void excluir(TopicoUser topico){
-		entityManager.remove(entityManager.merge(topico));
-	}
-	
-	public Topico consultar(Long topico_id){
-		return entityManager.getReference(Topico.class, topico_id);
-	}
-	
-	public List<Topico> listar(){
-		
-		String sql = "Select a from TopicoUser a order by a.user_id";
-		Query query = entityManager.createQuery(sql);
-		
-		return query.getResultList();
-	}
-}
+//package br.com.csm.DAO;
+//
+//import java.util.List;
+//
+//import javax.persistence.EntityManager;
+//import javax.persistence.Query;
+//
+//import br.com.csm.entidade.Topico;
+//import br.com.csm.entidade.TopicoUser;
+//
+//public class TopicoUserDAO {
+//
+//	private EntityManager entityManager;
+//	
+//	public TopicoUserDAO(EntityManager entityManager){
+//		this.entityManager = entityManager;
+//	}
+//	
+//	public void cadastrar(TopicoUser topico){
+//		entityManager.persist(topico);
+//	}
+//	
+//	public void alterar(TopicoUser topico){
+//		entityManager.merge(topico);
+//	}
+//	
+//	public void excluir(TopicoUser topico){
+//		entityManager.remove(entityManager.merge(topico));
+//	}
+//	
+//	public Topico consultar(Long topico_id){
+//		return entityManager.getReference(Topico.class, topico_id);
+//	}
+//	
+//	public List<Topico> listar(){
+//		
+//		String sql = "Select a from TopicoUser a order by a.user_id";
+//		Query query = entityManager.createQuery(sql);
+//		
+//		return query.getResultList();
+//	}
+//}
